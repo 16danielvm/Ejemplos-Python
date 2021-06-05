e76344829e534af073fb063cb6580ce3c9055600
Daniel <danielvm@unicauca.edu.co>
2021-06-05 17:11:29 -0500

    Correcion

--- Factorial.py
+++ Factorial.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''
 Ingrese un numero para determinar su factorial
 '''
@@ -15,24 +14,4 @@ for i in range(a,0,-1):
         print(i, end="")
     else:
         print(i,"x", end="")
-
-=======
-'''
-Ingrese un numero para determinar su factorial
-'''
-
-
-a = int(input("Ingrese el numero"))
-resultado = 1
-
-print(a,"! es igual a ", end="" )
-
-for i in range(a,0,-1):
-    resultado*=i
-    if i==1:
-        print(i, end="")
-    else:
-        print(i,"x", end="")
-
->>>>>>> 38d34672c7ec75fb6e8270988a8f22c942e32c22
 print(" =", resultado)
